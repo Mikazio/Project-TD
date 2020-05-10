7bf4023b844794a71dae2ba4efff69404225940f
Varin Titarat <varin.titarat@gmail.com>
2020-05-10 16:25:51 +0700

    [+]add firebase db

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,24 @@
+const { db } = require('../util/admin');
+
 exports.getAllTodos = (request, response) => {
-    todos = [
-        {
-            'id' : '1',
-            'title' : 'greeting',
-            'body' : 'Hello'
-        },
-        {
-            'id' : '2',
-            'title' : 'greeting',
-            'body' : 'Someone'
-        }
-    ]
-    return response.json(todos);
-}
+    db
+        .collection('todos')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let todos = [];
+            data.forEach((doc) => {
+                todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+                    body: doc.data().body,
+                    createAt: doc.data().createdAt,
+                });
+            });
+            return response.json(todos);
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({error: err.code});
+        });
+};
